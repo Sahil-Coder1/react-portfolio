679558f12280e9caa35532ee42e52f86d3dd8f4a
Sahil-Coder1 <26maysahil@gmail.com>
2024-09-08 19:38:51 +0530

    animation on scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
 import './index.css';
-import style from './style.css';
+import './style.css';
 import script from './script';
 import React, { useEffect } from 'react';
 import backtoTop from './image/backtoTop.png';
@@ -39,7 +38,6 @@ function App() {
             <head>
                 <title>Sahil Verma - Portfolio</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="stylesheet" href={style} />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
