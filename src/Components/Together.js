277da9d9071385a9a94dbccbccf85f1c2c027fa0
Sahil-Coder1 <26maysahil@gmail.com>
2024-12-02 22:45:47 +0530

    updates

--- src/Components/Together.js
+++ src/Components/Together.js
@@ -1,18 +1,25 @@
 const Together = () => {
-
-    const contactMe = () => {
-        window.open('mailto:26maysahil@gmail.com&subject=Lets Collabe&body=' + document.getElementById("desText").value);
-        document.getElementById("desText").value = "";
-    };
-    return(
-        <div className="together" id='together'>
-        <h1>Lets Design Together</h1>
-        <p className='desc'>Do you have a vision for a stunning website or app, but aren't sure how to bring it to life? Let's collaborate! I thrive on working closely with clients to translate their ideas into beautiful, user-friendly designs. We'll brainstorm together, explore different concepts, and refine everything until we create something truly remarkable.</p>
-        <div>
-            <input id='desText' type="text" placeholder='Your Idea' /><button onClick={contactMe}>Contact Me</button>
-        </div>
+  const contactMe = () => {
+    window.open(
+      "mailto:26maysahil@gmail.com&subject=Lets Collabe&body=" +
+        document.getElementById("desText").value
+    );
+    document.getElementById("desText").value = "";
+  };
+  return (
+    <div className="together" id="together">
+      <h1>Lets Design Together</h1>
+      <p className="desc">
+        Got a killer website or app idea? Let's turn your vision into reality.
+        Together, we'll craft stunning, user-friendly designs that captivate
+        your audience.
+      </p>
+      <div>
+        <input id="desText" type="text" placeholder="Your Idea" />
+        <button onClick={contactMe}>Contact Me</button>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Together;
